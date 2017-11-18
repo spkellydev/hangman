6e108f252ee6af38cacc8574471e2353098f7268
Sean Kelly <spkellydev@gmail.com>
2017-11-17 20:51:40 -0500

    adding image assets to game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 // create an array of words 
+function q(selector) {
+    return document.querySelector(selector);
+}
 const words = { 
                 words: ['purple', 'letter', 'snail', 'shark', 'sock', 'ham', 'chocolate'],
-                hints: ['color', 'part of alphabet', 'shelled insect', 'cartiliginous fish', 'on your feet', 'from a pig', 'delicious candy']
+                hints: ['the color _ _ _ _ _ _', 'part of alphabet', 'shelled insect', 'cartiliginous fish', 'goes on your feet', 'comes from a pig', 'delicious candy'],
+                images: ['../assets/images/colorPurple.jpg']
               };
 // choose word randomly
 let rand = Math.floor(Math.random() * words.words.length)
@@ -9,19 +13,34 @@ let chosen = words.words[rand];
 let chosenHint = words.hints[rand];
 let count = chosen.length;
 // create underscores based on words
-let _ = [],
-correct = [],
-wrong = [],
-underscores = document.querySelector('.underscores'),
-correctBlock = document.querySelector('.correct'),
-wrongBlock = document.querySelector('.wrong'),
-scoreBlock = document.querySelector('#score'),
-btn = document.querySelector('.btn'),
-hint = document.querySelector('.hint');
+let _ = [];
+
+let score = {
+  _: [],
+  correct: [],
+  wrong: []
+}
+
+let blocks = {
+  underscores: q('.underscores'),
+  correct: q('.correct'),
+  wrong: q('.wrong'),
+  score: q('#score'),
+  subtitle: q('.subtitle'),
+  img: q('.img')
+}
+
+let btn = q('.btn');
+let hint = q('.hint');
+
+//push to front end
 hint.innerHTML = chosenHint;
+var node = document.createElement("IMG");
+node.src = words.images[0]; 
+blocks.img.appendChild(node); 
 
 while (count > 0) {
-    _.push('_');
+    score._.push('_');
     count--;
 }
 
@@ -31,7 +50,7 @@ function commaReplace(t) {
 }
 
 //push underscores to the front end
-underscores.innerHTML = commaReplace(_);
+blocks.underscores.innerHTML = commaReplace(score._);
 
 function checkDuplicate (letter) {
     count = 0;
@@ -41,7 +60,7 @@ function checkDuplicate (letter) {
         }
     }
     if (count > 1) {
-        correct.push(' ');
+        score.correct.push(' ');
     }
 }
 
@@ -49,11 +68,11 @@ function checkLetter(letter) {
     for (var i = 0; i < chosen.length; i++) {
         if(letter == chosen.charAt(i)) {
             //you guessed right!
-            correct.indexOf(letter) === -1 ? correct.push(letter) : checkDuplicate(letter); //add empty space to account for correct index count
-            _[i] = letter;
+            score.correct.indexOf(letter) === -1 ? score.correct.push(letter) : checkDuplicate(letter); //add empty space to account for correct index count
+            score._[i] = letter;
         } 
     }
-    return _;
+    return score._;
 }
 
 // get users guess 
@@ -64,37 +83,49 @@ document.addEventListener('keypress', (e) => {
     if (chosen.indexOf(letter) > -1) {
         //push to the correct array
         //need to check if letter exists in word twice
-        //correct.indexOf(letter) === -1 ? correct.push(letter) : console.log('already guessed');
-        //moved to checkLetter function
-        //checkLetter(letter);
-        
-        //replace underscore with letter with commaReplace
-
         //console.log(`%c Correct: ${correct} `, 'background: #d2232a; color: #bada55');
-        //print remaining guess/correct guess
-        underscores.innerHTML = commaReplace(checkLetter(letter));
+        blocks.underscores.innerHTML = commaReplace(checkLetter(letter));
         //print to correct block
-        correctBlock.innerHTML = commaReplace(correct);
+        blocks.correct.innerHTML = commaReplace(score.correct);
         //compare the length of correct letters and the count of chosen letters
-        if (commaReplace(correct).length == chosen.length) {
-            alert('You win!');
-            setTimeout(function(){ location.reload(); }, 3000);
+        if (commaReplace(score.correct).length == chosen.length) {
+           return gameStatus('win');
         }
     } else {
         //push to the wrong word array
-        wrong.indexOf(letter) === -1 ? wrong.push(letter) : console.log('already guessed');
+        if (score.wrong.length < 10) {
+            score.wrong.indexOf(letter) === -1 ? score.wrong.push(letter) : console.log('already guessed');
+        }
         //print to wrong block
-        wrongBlock.innerHTML = commaReplace(wrong);
+        blocks.wrong.innerHTML = commaReplace(score.wrong);
         //update scoreblock
-        scoreBlock.innerHTML = 10 - wrong.length;
+        blocks.score.innerHTML = 10 - score.wrong.length;
         //console.log(`%c Wrong: ${wrong}`, 'background: #bada55; color: #d2232a');
-        if (wrong.length > 9) {
-            alert('You lose, try again!');
-            setTimeout(function(){ location.reload(); }, 3000);           
+        if (score.wrong.length > 9) {
+            return gameStatus('lose');          
         }
     }
 });
 
 function startGame() {
     location.reload();
+}
+function scoreBlock(state) {
+    if (state === 'win') {
+        return blocks.score.innerHTML = '😊';
+    } else {
+        return blocks.score.innerHTML = '🙁';
+    }
+}
+
+function gameStatus(state) {
+    if (state === 'win') {
+        scoreBlock(state);
+        blocks.subtitle.innerHTML = 'You Win!!! New Game starting soon!!';
+        setTimeout(function(){ startGame(); }, 1500);
+    } else {
+        scoreBlock(state);
+        blocks.subtitle.innerHTML = `You Lose!!! The Word was <span style="color: red;">${chosen.toUpperCase()}</span> New Game starting soon!!`;
+        setTimeout(function(){ startGame(); }, 2500);
+    }
 }
